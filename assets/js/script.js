33bc00bcbfca75c930c52201ca498dc5f551cd88
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-04-10 10:47:29 -0500

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,106 +1,44 @@
-var hour
 var currentHour
-var today
-var storedText
-var saveBtn = document.querySelectorAll("button");
-//var saveBtn = document.getElementById('fa-save');
-var formRow = document.getElementsByClassName('form-control')
-
-//var eventText = document.getElementById('textarea').value;
-var hour = document.getElementsByClassName("hour")
-var hourId = document.querySelectorAll('[id^=textarea]');
-//console.log(hourId);
+var btn = document.getElementsByClassName('btn');
 var gradDate = moment().format("dddd, MMMM Do");
+// populates time on page
 document.getElementById("currentDay").innerHTML = gradDate;
+// populates text field from local storage
+$("#h-8 [id^=textarea]").val(localStorage.getItem("h-8"));
+$("#h-9 [id^=textarea]").val(localStorage.getItem("h-9"));
+$("#h-10 [id^=textarea]").val(localStorage.getItem("h-10"));
+$("#h-11 [id^=textarea]").val(localStorage.getItem("h-11"));
+$("#h-12 [id^=textarea]").val(localStorage.getItem("h-12"));
+$("#h-13 [id^=textarea]").val(localStorage.getItem("h-14"));
+$("#h-14 [id^=textarea]").val(localStorage.getItem("h-14"));
+$("#h-15 [id^=textarea]").val(localStorage.getItem("h-15"));
+$("#h-16 [id^=textarea]").val(localStorage.getItem("h-16"));
+$("#h-17 [id^=textarea]").val(localStorage.getItem("h-17"));
 
-$("textarea").addClass("past");
-//var currentHour = 10
 var currentHour = moment().format("H");
-console.log('The current hour is',currentHour );
+// adds 'past' class to all rows on load
+$("textarea").addClass("past");
 
-// gets all 'hour' class elements and makes a node list
-var nodeList = document.getElementsByClassName('hour');
-currentHour = parseInt(currentHour)
-
-console.log(typeof currentHour)
-// var i = document.getElementById("textarea20")
-//let item = nodeList[i].textContent.replace(/[^0-9]/g,'');
-
-var textarea = $('#textarea8');
-console.log(textarea)
-
+// changes textarea background depending on currentHour
 for (let i = 8; i <= 17; i++) {
   var textarea = $('#textarea' + i);
-  console.log(textarea)
+ 
 if (i > currentHour) {
   $(textarea).removeClass('past').addClass('future');
 }
 else if (i == currentHour) {
-  console.log('current');
   $(textarea).removeClass('past').addClass('present');
-}
-}
+}}
 
+// saves to loacal storage
+$(btn).on("click", function () {
+// gets value from textarea
+  var text = $(this).siblings('.txtarea').val();
+  // gets id name
+  var id = $(this).parent().attr("id");
+// adds id name with textarea strings to local storage
+localStorage.setItem(id, text);
 
-/*
-for (let i = 0; i < nodeList.length; i++) {
-  let item = nodeList[i].textContent.replace(/[^0-9]/g,'');
+})
 
-console.log(item);
-
-if(currentHour < item){
-  $(formRow).removeClass('past').addClass('future');
-  //console.log("future");
-} else if(currentHour == item){
-  $(formRow).removeClass('past').addClass('current');
- // console.log("current");
-}}}
-
-*/
-
-
-renderStored();
-save();
-function save() {
-  var eventText = $('[id^=textarea]').val();
-  localStorage.setItem('enteredText', eventText);
-  console.log(eventText);
-}
-
-$(saveBtn).click(function () {
-  save();
-  //disable();
-});
-
-function renderStored() {
-  var eventText = $('#textarea').val();
-  var storedText = localStorage.getItem('enteredText');
-  //eventText.textContent = storedText;
-  console.log(storedText)
-  $('#textarea').text(storedText);
-}
-/*
-function disable() {
-  $("[id^=textarea]:input").prop('readonly', true);
-  $("button-addon2").on("click", function (event) {
-
-  })
-}
-
-*/
-
-var divs = document.getElementsByTagName('[id^=textarea]');
-console.log(divs);
-for(var i = 0; i < divs.length; i++){
-  //do something to each div like
-  divs[i].innerHTML = "something new...";
-}
-
-if (hourId < currentHour) {
-$(hourId).addClass('future');
-document.write("future");
-} else {
-  $(hourId).addClass('past');
-  document.write("past");
-} 
 
